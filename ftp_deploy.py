dbd95617e23a26977f20d7eb94aa4221eef8279a
RandomlyKnighted <thughes3@jsu.edu>
2016-05-31 21:31:46 -0500

    WIP - testing python deployment script

--- ftp_deploy.py
+++ ftp_deploy.py
@@ -23,6 +23,6 @@ sftp = pysftp.Connection( server, username=username, password=password )
 try:
     print "Starting recursive copy of files"
     path = os.getcwd()
-    sftp.put_r( path, 'test', preserve_mtime=True)
+    sftp.put_r( "./", 'test', preserve_mtime=True)
 except:
     print "Failed to copy file(s) to the server"
