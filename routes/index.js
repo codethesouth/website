f69414e5462463cce16b01ee69bf80d581dfc4af
Tyler Hughes <thughes@randomlyknighted.com>
2016-06-08 21:39:49 -0500

    Removed unneeded libraries

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+﻿var express = require("express");
+var router = express.Router();
+
+router.get("/", function (request, response) {
+    response.render("index", { title: "CodeTheSouth" });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+﻿var express = require("express");
+var path = require("path");
+var routes = require("./routes/index");
+
+var app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "vash");
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", routes);
+
+var server = app.listen(app.get("port"));
+
+module.exports = app;
