4c7a8fe4bf4bb1cb036871efc8d6356965de3269
Tyler Hughes <thughes@randomlyknighted.com>
2016-07-06 08:53:29 -0500

    WIP - pulled values from the contact form and started initial setup for the submission

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,14 @@
  *  API Key: AIzaSyBSHBj2OKvpPnwXyKC5AS6Il2mBVV-YXgU
  */
 
+var form = document.querySelector("#contactForm");
+
+form.addEventListener("submit", function (event) {
+    // Prevent the page from reloading when the form is submitted
+    event.preventDefault();
+
+    var name = document.querySelector("#nameLabel").value;
+    var email = document.querySelector("#emailLabel").value;
+    var message = document.querySelector("#msgTextarea").value;
+    var reason = document.querySelector("#formSubmissionOption > option").value;
+});
