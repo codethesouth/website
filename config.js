2e302ff67d1fb421f7cab5edd3ae5c4bd500e908
PallasMedia <tyler@tylerhughes.info>
2020-10-31 16:07:21 -0500

    Fixed production build

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/favicon.ico',
+  manifestIcon: 'src/assets/img/codethesouth.svg',
   pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Code the South',
   subHeading: 'Building software solutions for the community.',
